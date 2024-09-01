6913fa5648b8a85fed47b792ed347326422cb910
andeya <andeyalee@outlook.com>
2024-09-01 11:50:09 +0800

    chore: update smux

--- logimesh/src/transport/smux/mod.rs
+++ logimesh/src/transport/smux/mod.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 use futures::{select, FutureExt};
 use std::collections::HashMap;
 use std::sync::Arc;
@@ -6,6 +7,8 @@ use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt, DuplexStream
 use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf};
 use tokio::net::TcpStream;
 use tokio::sync::Mutex;
+use tracing::warn;
+
 pub struct StreamMux {
     /// The buf max size.
     /// Default: 1024.
@@ -22,7 +25,7 @@ pub(crate) struct Stream {
 
 impl StreamMux {
     pub fn new(conn: TcpStream) -> Self {
-        let (mut r, mut w) = conn.into_split();
+        let (r, w) = conn.into_split();
         let duplex_map: Arc<Mutex<HashMap<u16, DuplexStream>>> = Default::default();
         Self::start_receiving(r, duplex_map.clone());
         Self {
@@ -43,17 +46,23 @@ impl StreamMux {
     }
     fn start_receiving(mut r: OwnedReadHalf, duplex_map: Arc<Mutex<HashMap<u16, DuplexStream>>>) {
         tokio::spawn(async move {
+            let mut buf = Vec::new();
             loop {
-                if let Err(err) = Self::read_pack(&mut r, &duplex_map).await {
-                    todo!()
+                if let Err(err) = Self::read_pack(&mut r, &mut buf, &duplex_map).await {
+                    warn!("[LOGIMESH] Error occurred when reading data from the connection: {}", err);
                 }
             }
         });
     }
-    async fn read_pack(conn: &mut OwnedReadHalf, duplex_map: &Mutex<HashMap<u16, DuplexStream>>) -> Result<(), std::io::Error> {
+    async fn read_pack(conn: &mut OwnedReadHalf, buf: &mut Vec<u8>, duplex_map: &Mutex<HashMap<u16, DuplexStream>>) -> Result<(), std::io::Error> {
         let comp_port = conn.read_u16().await?;
         let pack_size = conn.read_u32().await?;
-
+        let mut duplex_map = duplex_map.lock().await;
+        let ds = duplex_map.get_mut(&comp_port).unwrap();
+        buf.resize(pack_size as usize, 0);
+        conn.read_exact(buf).await?;
+        ds.write_all_buf(&mut buf.as_slice()).await?;
+        ds.flush().await?;
         Ok(())
     }
     pub async fn new_stream(&self, comp_port: u16) -> Stream {
@@ -79,15 +88,34 @@ impl AsyncRead for Stream {
 }
 
 impl AsyncWrite for Stream {
-    fn poll_write(self: std::pin::Pin<&mut Self>, cx: &mut Context<'_>, buf: &[u8]) -> Poll<Result<usize, std::io::Error>> {
-        todo!()
+    fn poll_write(self: std::pin::Pin<&mut Self>, _: &mut Context<'_>, buf: &[u8]) -> Poll<Result<usize, std::io::Error>> {
+        let f = async {
+            let mut write_half = self.write_half.lock().await;
+            write_half.write_u16(self.comp_port).await?;
+            write_half.write_u32(buf.len() as u32).await?;
+            write_half.write_all(buf).await?;
+            Ok::<usize, std::io::Error>(2 + 4 + buf.len())
+        };
+        select! {
+            r = f.fuse() => {
+                Poll::Ready(r)
+            },
+            default => Poll::Pending,
+        }
     }
 
-    fn poll_flush(self: std::pin::Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), std::io::Error>> {
-        todo!()
+    fn poll_flush(self: std::pin::Pin<&mut Self>, _: &mut Context<'_>) -> Poll<Result<(), std::io::Error>> {
+        // TODO: Reduce the calling frequency
+        select! {
+            r = async{self.write_half.lock().await.flush().await}.fuse() => {
+                Poll::Ready(r)
+            },
+            default => Poll::Pending,
+        }
     }
 
-    fn poll_shutdown(self: std::pin::Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), std::io::Error>> {
-        todo!()
+    fn poll_shutdown(self: std::pin::Pin<&mut Self>, _: &mut Context<'_>) -> Poll<Result<(), std::io::Error>> {
+        // There is no requirement for active closure.
+        Poll::Ready(Ok(()))
     }
 }
